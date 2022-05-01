838673b33959ca1c7916ee6a5de3c1071c1fff14
Andy <scp.andyliu@gmail.com>
2022-05-01 19:43:02 +1200

    c

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow } = require('electron')
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600
-    })
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
 
-    win.loadFile('index.html')
+  win.loadFile('index.html')
 }
 
-app.whenReady().then(() => {
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
+  }
 })
